f891013c61e24458426fa083bcfabcff3d94b923
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-08-06 00:23:01 +0100

    add randChars variable to randAlphaNum function

--- AP101-10-5.js
+++ AP101-10-5.js
@@ -1,9 +1,7 @@
 function randAlphaNum() {
-const alphas = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z' ]
+const alphas = [ 'A', 'B', 'C', 'D', 'E', 'F']
 
-const randAlpha = () => alphas[Math.floor(Math.random() * (25 - 0 + 1)) + 0];
+const randAlpha = () => alphas[Math.floor(Math.random() * (5 - 0 + 1)) + 0];
 const randNum = () => Math.floor(Math.random() * 10);
 const firstTwoAlph = randAlpha() + randAlpha();
 const firstDigit = randNum();
@@ -11,8 +9,9 @@ const secDigit = randNum();
 const lastTwoAlph = randAlpha() + randAlpha();
 const thirdDigit = randNum();
 const lastDigit = randNum();
+const randChars = firstTwoAlph + firstDigit + secDigit + lastTwoAlph + thirdDigit + lastDigit;
 
-return firstTwoAlph + firstDigit + secDigit + lastTwoAlph + thirdDigit + lastDigit;
+return randChars;
 }
 
 // testing
